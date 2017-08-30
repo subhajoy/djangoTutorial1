223d4df88900e9ed5f305b107109f62980eaf3d7
subhajoy <subhajoydas94@gmail.com>
2017-08-30 20:09:10 +0530

    Chapter 5 complete, created models

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 def index1(request):
