c8b55daf7f5ec2496dd788a23be54d75a2b0ee56
subhajoy <subhajoydas94@gmail.com>
2017-08-31 12:18:24 +0530

    Chapter 6 done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 
 def index1(request):
     return HttpResponse("Rango says lol Hey there partner!")
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -24,6 +24,7 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
